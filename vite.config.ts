a79e231f2a9d9dad289d0ab39cb5110a5e802e6f
pr241 <pr241@waikato.students.ac.in>
2026-05-12 14:50:23 +0530

    config change

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,14 @@ export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
   },
+  vite: {
+    server: {
+      port: 8081,
+      strictPort: true,
+    },
+    preview: {
+      port: 8081,
+      strictPort: true,
+    },
+  },
 });
